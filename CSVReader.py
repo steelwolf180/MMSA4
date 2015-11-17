bd8555c92d7f4cc6d2fd8ac2c213d6ccb3276dee
steelwolf180 <steelwolf180@gmail.com>
2015-11-17 21:24:56 +0800

    Find common words in tag

--- CSVReader.py
+++ CSVReader.py
@@ -1,48 +1,46 @@
 __author__ = 'max'
 import csv  # imports the csv module
-import sys  # imports the sys module
+from collections import defaultdict #special dict that returns default values whenever a key is selected from the dict
 
-# To be able to read csv formated files, we will first have to import the
-# csv module.
-import csv
+
 # open csv file
 tags = open('tags.csv', "rt", encoding="utf-8")
-photos = open('photos.csv', "rt", encoding="latin-1")
+#photos = open('photos.csv', "rt", encoding="latin-1")
 photos_tag = open('photos_tags.csv', "rt", encoding="utf-8")
 
-#photos table table
-p_id = []
-p_width = []
-p_height = []
-p_title = []
-p_upload = []
+#declareation
+photo_tag = defaultdict(list) #photos tag table
+temp_list = [] #tempoary list
+tag_list =[] #to store list of tags
+matrix_tuple = []  #blank matrix list with tuple
+matrix_dic = {}
 
-#photos tag table
-pt_id = []
-pt_tag = []
-
-#tags table table
-t_tag = []
-t_num = []
-
-with tags as f:
+#Reads from CSV file and add to a list
+with photos_tag as f: #reads photo tags from photo tags table
     reader = csv.reader(f)
     for val in reader:
-        t_tag.append(val[0])
-        t_num.append(val[1])
+        temp_list.append(val)
 
-with photos as f:
+with tags as f: #gets the list of tags from tag table
     reader = csv.reader(f)
     for val in reader:
-        p_id.append(val[0])
-        p_width.append(val[1])
-        p_height.append(val[2])
-        p_title.append(val[3])
-        p_upload.append(val[4])
+        tag_list.append(val[0])
 
-with photos_tag as f:
-    reader = csv.reader(f)
-    for val in reader:
-        pt_id.append(val[0])
-        pt_tag.append(val[1])
-print("Who am i???")
+#converts list into a dictionary to include image with their own tags
+for image, tag in temp_list:
+    photo_tag[tag].append(image)
+
+temp_list.clear()
+
+for x in tag_list:
+    for y in tag_list:
+        matrix_tuple.append((x,y))
+
+for i in tag_list:
+    for j in tag_list:
+        matrix_dic[(i),(j)] = 0
+
+'''
+for key in sorted(matrix_dic):
+   print("key: %s , value: %s" % (key, matrix_dic[key]))
+'''

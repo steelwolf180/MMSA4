9431580983967b89c954eef371b845710fdab8b8
steelwolf180 <steelwolf180@gmail.com>
2015-11-19 02:27:38 +0800

    Completed Task 1 part 1

--- CSVReader.py
+++ CSVReader.py
@@ -14,7 +14,8 @@ by_imageid = defaultdict(list) #create dict based upon photo id from photos tag 
 temp_list = [] #tempoary list
 tag_list =[] #to store list of tags
 matrix_tuple = []  #blank matrix list with tuple
-matrix_dic = {} #blank matrix dictionary to add values
+matrix_dic =  defaultdict(int) #blank matrix dictionary to add values
+temp_dic = defaultdict(int)
 
 def read_CSV():
     #Reads from CSV file and add to a list
@@ -29,24 +30,38 @@ def read_CSV():
             tag_list.append(val[0])
 
 def gen_matrix():
+    count = 0
     #create matrix with tags
-    for x in tag_list:
-        for y in tag_list:
+    for x in sorted(tag_list):
+        for y in sorted(tag_list):
             matrix_tuple.append((x,y))
 
-    #print(matrix_tuple) #print matrix table with each key for the coordinates
+    #generate blank matrix table
+    for i in sorted(tag_list):
+        for j in sorted(tag_list):
+            matrix_dic[(i),(j)]
 
-    #generate matrix table
-    for i in tag_list:
-        for j in tag_list:
-            matrix_dic[(i),(j)] = 0
-    '''
-    #to print matrix dic
-    for key in sorted(matrix_dic):
-       print("key: %s , value: %s" % (key, matrix_dic[key]))
-    '''
+    #update matrix table with co-occurrence  for each individual keywords
+    for i in sorted(tag_list):
+        for j in sorted(tag_list):
+            for id in by_imageid:
+                if j == i:
+                    temp_dic[(i),(j)] = 0
+                    matrix_dic.update(temp_dic)
+                    break
+                elif j in by_imageid[id] and i in by_imageid[id]:
+                    count=matrix_dic[(i),(j)]
+                    count=count+1;
+                    matrix_dic[(i),(j)] = count
+                else:
+                    count = 0
+
+    #print matrix table with each key for the coordinates
+    for k,v in sorted(matrix_dic.items()):
+        print("keys:",k,"values:",v)
 
 def tag_relationships():
+
     #displays all the tag id that a photo is tagged based on tag word/number
     for image, tag in sorted(temp_list): #converts list into a dictionary to include image with their own tags
         by_words[tag].append(image)
@@ -54,14 +69,13 @@ def tag_relationships():
     #displays all tag words/number that a photo is tagged based on photo id
     for image, tag in sorted(temp_list):#converts list into a dictionary to include image with their own tags
         by_imageid[image].append(tag)
+
     temp_list.clear()
 
 def start():#calls all the other function in the program
     read_CSV()
-    gen_matrix()
     tag_relationships()
+    gen_matrix()
+
 
 start() #start of the python program
-#print(photo_tag['red'])
-
-print(by_imageid)
